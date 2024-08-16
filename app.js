1e215c4382ca27f5aeccbdb07eb245810971a53a
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-16 22:33:40 +0530

    chat app

--- app.js
+++ app.js
@@ -41,3 +41,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/api/videoCall', require('./routes/videoCallRoutes'));
+
