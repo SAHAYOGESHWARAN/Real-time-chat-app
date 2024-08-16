0bf1328249052426d04afc974404f9564bdd2983
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-16 22:49:58 +0530

    chat app

--- app.js
+++ app.js
@@ -1,45 +1,27 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
-dotenv.config();
-
+const path = require('path');
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-connectDB();
+const port = 3000; // You can use any port number you prefer
 
 // Middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
 
 // Routes
+app.use('/api/media', require('./routes/mediaRoutes'));
+app.use('/api/videoCall', require('./routes/videoCallRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/chat', require('./routes/chatRoutes'));
-app.use('/api/settings', require('./routes/settingsRoutes'));
-app.use('/api/media', require('./routes/mediaRoutes'));
-app.use('/api/status', require('./routes/statusRoutes'));
 
-// Socket.io connection
-io.on('connection', (socket) => {
-  console.log('New client connected');
+// Serve HTML pages
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/welcome.html')));
+app.get('/country-select', (req, res) => res.sendFile(path.join(__dirname, 'public/country-select.html')));
+app.get('/otp', (req, res) => res.sendFile(path.join(__dirname, 'public/otp.html')));
+app.get('/chat', (req, res) => res.sendFile(path.join(__dirname, 'public/chat.html')));
+app.get('/settings', (req, res) => res.sendFile(path.join(__dirname, 'public/settings.html')));
+app.get('/status', (req, res) => res.sendFile(path.join(__dirname, 'public/status.html')));
 
-  socket.on('chatMessage', (msg) => {
-    io.emit('chatMessage', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
-app.use('/api/videoCall', require('./routes/videoCallRoutes'));
-
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
