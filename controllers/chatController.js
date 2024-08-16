3c773bbd2246d0a6ac5dfc4929daebc185833ea7
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-16 22:13:06 +0530

    chat app

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,9 +1,26 @@
 const Message = require('../models/message');
 
-exports.getMessages = async (req, res) => {
+exports.sendMessage = async (req, res) => {
+  const { sender, receiver, message, media } = req.body;
   try {
-    const messages = await Message.find().sort({ timestamp: 1 });
-    res.json(messages);
+    const newMessage = new Message({ sender, receiver, message, media });
+    await newMessage.save();
+    res.status(201).json({ message: 'Message sent' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getMessages = async (req, res) => {
+  const { sender, receiver } = req.query;
+  try {
+    const messages = await Message.find({
+      $or: [
+        { sender, receiver },
+        { sender: receiver, receiver: sender },
+      ],
+    }).sort({ timestamp: 1 });
+    res.status(200).json(messages);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
