a11388d606da0f5133a67f47094c3b384faa85d2
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-17 08:45:01 +0530

    chat app

--- models/status.js
+++ models/status.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose');
 
+// Define the schema for the Status model
 const StatusSchema = new mongoose.Schema({
-  user: String,
-  statusText: String,
-  media: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  statusText: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  media: {
+    type: String,
+    trim: true
+  },
   timestamp: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
+// Create the model from the schema
 module.exports = mongoose.model('Status', StatusSchema);
