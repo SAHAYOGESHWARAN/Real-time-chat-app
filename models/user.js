002482b9d807d2907d47c483717039d3a5fe5bdd
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-17 08:46:12 +0530

    chat app

--- models/user.js
+++ models/user.js
@@ -1,12 +1,37 @@
 const mongoose = require('mongoose');
 
+// Define the schema for the User model
 const UserSchema = new mongoose.Schema({
-  country: String,
-  phone: String,
-  otp: String,
-  username: String,
-  status: String,
-  avatar: String,
+  country: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  otp: {
+    type: String,
+    trim: true
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  status: {
+    type: String,
+    trim: true
+  },
+  avatar: {
+    type: String,
+    trim: true
+  }
 });
 
+// Create the model from the schema
 module.exports = mongoose.model('User', UserSchema);
